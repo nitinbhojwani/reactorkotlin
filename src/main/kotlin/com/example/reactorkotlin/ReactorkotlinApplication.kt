03467105a1f821b5839af031c8a06e235c9caf0a
Nitin Bhojwani <nitinbhojwani@outlook.com>
2020-07-18 11:54:02 +0530

    generate the project at start.spring.io using Spring Initializr

---
+++ src/main/kotlin/com/example/reactorkotlin/ReactorkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.reactorkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactorkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactorkotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/reactorkotlin/ReactorkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.reactorkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactorkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
